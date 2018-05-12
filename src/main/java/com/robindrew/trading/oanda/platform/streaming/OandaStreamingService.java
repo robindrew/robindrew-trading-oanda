24b5d81acc91b99f29aadab9c351f22d5f75fe11
Robin Drew <robin.drew@gmail.com>
2018-05-12 14:43:35 +0100

    Major refactor of PCF/PTF file management

--- src/main/java/com/robindrew/trading/oanda/platform/streaming/OandaStreamingService.java
+++ src/main/java/com/robindrew/trading/oanda/platform/streaming/OandaStreamingService.java
@@ -1,9 +1,15 @@
 package com.robindrew.trading.oanda.platform.streaming;
 
-import com.robindrew.trading.oanda.IOandaInstrument;
-import com.robindrew.trading.platform.streaming.StreamingService;
+import static com.robindrew.trading.provider.TradingProvider.OANDA;
 
-public class OandaStreamingService extends StreamingService<IOandaInstrument> {
+import com.robindrew.trading.oanda.IOandaInstrument;
+import com.robindrew.trading.platform.streaming.AbstractStreamingService;
+
+public class OandaStreamingService extends AbstractStreamingService<IOandaInstrument> {
+
+	public OandaStreamingService() {
+		super(OANDA);
+	}
 
 	@Override
 	public boolean subscribe(IOandaInstrument instrument) {
